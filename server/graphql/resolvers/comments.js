0f5aa45f62e3fcd7aab72bfa27180f7187ada008
overaction <kmc04@naver.com>
2021-03-16 00:04:04 +0900

    8. 코멘트 삭제

--- server/graphql/resolvers/comments.js
+++ server/graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server-errors');
+const { UserInputError, AuthenticationError } = require('apollo-server-errors');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth');
 
@@ -28,6 +28,26 @@ module.exports = {
             catch(err) {
                 throw new UserInputError('포스트를 찾을 수 없습니다')
             }
-        } 
+        },
+        async deleteComment(_,{postId, commentId},context) {
+            const user = checkAuth(context);
+            
+            try {
+                const post = await Post.findById(postId);
+                const commentIdx = post.comments.findIndex(c => c.id === commentId);
+                if(post.comments[commentIdx].username === user.username) {
+                    post.comments.splice(commentIdx,1);
+                    await post.save();
+                    
+                    return post;
+                }
+                else {
+                    throw new AuthenticationError('허용되지 않은 접근입니다')
+                }
+            }
+            catch(err) {
+                throw new UserInputError('포스트 또는 댓글이 존재하지 않습니다');
+            }
+        }
     }
 }
